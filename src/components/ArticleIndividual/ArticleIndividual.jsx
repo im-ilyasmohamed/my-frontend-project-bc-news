82f595e6c0d917334f2bc86034f09a1a61249ce4
im-ilyasmohamed <im.ilyasmohamed@gmail.com>
2024-09-18 09:59:54 +0100

    refactored changes from comments

--- src/components/ArticleIndividual/ArticleIndividual.jsx
+++ src/components/ArticleIndividual/ArticleIndividual.jsx
@@ -1,29 +1,41 @@
-function ArticleIndividual({ loading, currentArticleIndividual }) {
-  function helperFunction() {
-    console.log(currentArticleIndividual, "individual article page");
-  }
-  helperFunction();
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import apiClient from "../../util/my-axios-api";
+function ArticleIndividual() {
+  const { article_id } = useParams();
+  const [article, setArticle] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    // Fetch the article data using the article_id
+    apiClient
+      .get(`/api/articles/${article_id}`)
+      .then((response) => {
+        const articleItem = response.data.articleItem[0];
+        setArticle(articleItem);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching article:", error);
+        setLoading(false);
+      });
+  }, [article_id]); // Runs when article_id changes
 
-  // could refactor to add loading states
   return (
     <>
       {/* api Load states renders */}
       {loading ? (
         <p>Loading article...</p>
-      ) : currentArticleIndividual ? (
-        <div
-          id="article-card-homepage"
-          key={currentArticleIndividual.article_id}
-        >
+      ) : article ? (
+        <div id="article-card-homepage" key={article.article_id}>
           <img
-            src={currentArticleIndividual.article_img_url}
+            src={article.article_img_url}
             alt="Article"
             className="img-article-card-homepage"
           />
           <div>
-            <h3>{currentArticleIndividual.title}</h3>
-            <h4>{currentArticleIndividual.topic}</h4>
-            <p>{currentArticleIndividual.body}</p>
+            <h3>{article.title}</h3>
+            <h4>{article.topic}</h4>
+            <p>{article.body}</p>
           </div>
         </div>
       ) : (
